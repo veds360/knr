86c2bfe9406471b68591df0f327ef1a2246601d0
Ved <vsunetra18@gmail.com>
2025-12-20 12:19:49 +0530

    chapter_1

---
+++ chapter_1/1.7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define lower 0
+#define upper 300
+#define step 20
+int main(){
+    
+    // for(int fahr=lower;fahr<=upper;fahr+=step){
+    //     printf("%d\t%.1f\n",fahr,(5.0)*(fahr-32)/9.0);
+    // }
+
+    // char c= getchar();
+    // printf("%c\n",c);
+
+   int c;
+   printf("%d\n",EOF);
+}
